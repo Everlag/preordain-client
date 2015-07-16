ecb02ab336e819f0f1f877b4a36b537702d765df
Everlag <kyleveb@gmail.com>
2015-07-16 17:22:14 -0600

    Added preordain-set-view.
    This is a full set view combining preordain-set-info and
    preordain-set-gallery.

---
+++ app/elements/preordain-set-view/preordain-set-view.js
@@ -0,0 +1,24 @@
+(function () {
+  Polymer({
+    // A full set's worth of information.
+    //
+    // Dispatches the 'request-card' event when a user inquires
+    // about more detail for a specific card.
+    is: 'preordain-set-view',
+    properties: {
+      name: {
+        type: String,
+        value: ' ',
+        notify: true,
+        observer: '_facetChanged',
+      },
+    },
+    attached: function() {
+    },
+    _facetChanged: function(){
+      // Ensure we are dealing only with valid set names
+      if (!setList.has(this.name)) throw 'invalid set name';
+    },
+
+  });
+})();
