721dcb2ab1b1b776fb6471032844f61a5f6b3083
Everlag <kyleveb@gmail.com>
2015-08-11 12:20:45 -0600

    preordain-trade-view skeleton
    The full trade view combines a user's history as well as a trade
    input positioned to the right of the history that follows the user as
    they scroll.

---
+++ app/elements/preordain-trade-view/preordain-trade-view.js
@@ -0,0 +1,21 @@
+(function () {
+  Polymer({
+    is: 'preordain-trade-view',
+    properties: {
+      name: {
+        type: String,
+        value: ' ',
+        notify: true,
+        observer: '_nameChanged',
+      },
+      _big: {
+        type: Boolean,
+        value: false,
+      }
+    },
+    _nameChanged: function(){
+      
+      console.log('aha', this.name);
+    },
+  });
+})();
