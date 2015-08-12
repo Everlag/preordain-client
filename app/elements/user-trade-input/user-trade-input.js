faa5bc8a3e86f3905e65d656fa6cff3b2a6658e7
Everlag <kyleveb@gmail.com>
2015-08-11 20:20:58 -0600

    user-trade-input card input and basic style

---
+++ app/elements/user-trade-input/user-trade-input.js
@@ -0,0 +1,32 @@
+(function () {
+  Polymer({
+    is: 'user-trade-input',
+    properties: {
+      // The items in the trade already present
+      seed: {
+        type: Array,
+        value: ()=> [],
+        notify: true,
+        observer: '_seedChanged',
+      },
+      // The choices the user has made in this trade
+      changes: {
+        type: Array,
+        value: ()=> [],
+        notify: true,
+      },
+      _workingCard: {
+        type: String,
+        value: ' ',
+      }
+    },
+    _seedChanged: function(){
+
+      console.log('ahaha!', this.seed);
+    },
+    _cardChosen: function({detail: {choice} }) {
+      this._workingCard = choice;
+    }
+
+  });
+})();
