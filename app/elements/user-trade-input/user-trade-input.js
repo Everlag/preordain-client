f4f9b15e24778e41cca2488f7212a6ede16207f3
Everlag <kyleveb@gmail.com>
2015-08-12 15:22:12 -0600

    user-trade-input scrolls on overflow
    This prevents cards with many sets from overflowing the page and making
    the input impossible to use for that card.

--- app/elements/user-trade-input/user-trade-input.js
+++ app/elements/user-trade-input/user-trade-input.js
@@ -18,13 +18,22 @@
       _workingCard: {
         type: String,
         value: ' ',
-      }
+      },
+      _cardSelected: {
+        type: Boolean,
+        value: false,
+        computed: '_isCardSelected(_workingCard)',
+      },
     },
     _seedChanged: function(){
 
     },
     _cardChosen: function({detail: {choice} }) {
       this._workingCard = choice;
+    },
+    _isCardSelected: function(card) {
+      if (card.trim().length > 0) return true;
+      return false;
     }
 
   });
