fc064b1fce0307aed8e6234dcc69e53d294690bc
Everlag <kyleveb@gmail.com>
2015-08-14 20:06:16 -0600

    preordain-trade-view hides input unless a trade to append to is selected
    This includes when a user has finished appending, the trade input goes
    away.

--- app/elements/preordain-trade-view/preordain-trade-view.js
+++ app/elements/preordain-trade-view/preordain-trade-view.js
@@ -11,10 +11,26 @@
       _big: {
         type: Boolean,
         value: false,
+      },
+      _selected: { // If a trade is selected
+        type: Boolean,
+        value: false,
       }
     },
     _nameChanged: function(){
       
     },
+    // A trade has been selected, we show the input
+    _tradeSelected: function({detail}) {
+      this._selected = true;
+    },
+    // The currently viewed trade is done
+    _tradeDone: function() {
+      // Set the user-trades to not display any trade as
+      // selected
+      this.$.history.Selected = 0;
+      this._selected = false;
+    }
+
   });
 })();
