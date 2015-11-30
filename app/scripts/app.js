bccdfa23914917579a4f512e3498ff82fe8a00b8
Everlag <kyleveb@gmail.com>
2015-11-29 20:26:10 -0700

    optimized first render
    Previously, the user would be greeted with a blank display until
    absolutely everything loaded. This felt extremely bad.
    
    Implements strategy in https://www.youtube.com/watch?v=Yr84DpNaMfk and
    https://gist.github.com/ebidel/1ba71473d687d0567bd3 by ebidel.
    
    This adds in a simple loading screen that exploits the favicon in order
    to replace blank white screen. Dramatically improves the experience.
    
    Potentially breaks iOS, needs more testing.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,10 +9,50 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 
 let started;
 
+let finishLazyLoading = ()=>{
+  // 6. Fade splash screen, then remove.
+  let onImportLoaded = function() {
+    let loadEl = document.getElementById('splash');
+    loadEl.addEventListener('transitionend', loadEl.remove);
+
+    document.body.classList.remove('loading');
+
+    // App is visible and ready to load some data!
+  };
+
+  let link = document.querySelector('#bundle');
+
+  // 5. Go if the async Import loaded quickly. Otherwise wait for it.
+  // crbug.com/504944 - readyState never goes to complete until Chrome 46.
+  // crbug.com/505279 - Resource Timing API is not available until Chrome 46.
+  if (link.import && link.import.readyState === 'complete') {
+    onImportLoaded();
+  } else {
+    link.addEventListener('load', onImportLoaded);
+  }
+}
+
 (function(document) {
   'use strict';
 
   // Record when we start execution so we can determine startup time
   started = performance.now();
+
+  // 4. Conditionally load the webcomponents polyfill if needed by the browser.
+  // This feature detect will need to change over time as browsers implement
+  // different features.
+  let webComponentsSupported = ('registerElement' in document &&
+      'import' in document.createElement('link') &&
+      'content' in document.createElement('template'));
+
+  if (!webComponentsSupported) {
+    let script = document.createElement('script');
+    script.async = true;
+    script.src = webcomponentsPolyfill;
+    script.onload = finishLazyLoading;
+    document.head.appendChild(script);
+  } else {
+    finishLazyLoading();
+  }
 
 })(document);
