f6b99069cde022db7ea35d925d1e58359a80c76e
Everlag <kyleveb@gmail.com>
2015-12-12 18:57:04 -0700

    added ability to restart view-notify after halting

--- app/elements/view-notify/view-notify.js
+++ app/elements/view-notify/view-notify.js
@@ -12,14 +12,24 @@ window.ViewNotifyBehavior = {};
 //                If _viewNotify returns a truthy value, checking
 //                is halted.
 //
-// Additionally, elements must
-// leave _ViewNotifyCheck available for internal use of view-notify.
+// Additionally, elements must leave all properties and methods
+// declared in this behavior unchanged except the special
+// case of _viewNotify.
+//
 ViewNotifyBehavior = {
+  properties: {
+    // Handle of next async check, explicitly null
+    // when we have none scheduled
+    _checkHandle: {
+      type: Number,
+      value: null,
+    }
+  },
   // Start the async check chain
   attached: function(){
-    this.async(this._ViewNotifyCheck, 100);
+    this._ViewNotifyStartCheck();
   },
-  // Perform a cheap check then callback if visible
+  // Perform a cheap check then callback if visible.
   _ViewNotifyCheck: function() {
 
     // Check and fire reserved callback if available.
@@ -28,11 +38,25 @@ ViewNotifyBehavior = {
     // any further checking, so we check that.
     if($(this).isOnScreen(0.1, 0.1)){
       let stop = this._viewNotify();
-      if (stop) return;
+      if (stop){
+        this._checkHandle = null;
+        return;
+      }
     }
 
     // Set up the next check
-    this.async(this._ViewNotifyCheck, 100);
+    this._checkHandle = this.async(this._ViewNotifyCheck, 100);
+  },
+  // Begins checking for visibility if we aren't already visible.
+  //
+  // Use this to restart checking if we previously halted checking
+  // due to the callback return value.
+  //
+  // Calling when a check is already scheduled results in a nop
+  _ViewNotifyStartCheck: function(){
+    if (this._checkHandle) return;
+
+    this._checkHandle = this.async(this._ViewNotifyCheck, 100);
   },
   // Make a fuss if the callback we need isn't present.
   _viewNotify: function(){
